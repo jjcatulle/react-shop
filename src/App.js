255502301e3dff5b406d537cbd699e39626db882
Jean Catulle <jjcatulle389@gmail.com>
2019-10-01 17:49:37 -0400

    added withrouter for navigaton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch,Route } from "react-router-dom";
 
 function App() {
   return <div className="">
-    <HomePage/>
+    <Switch> //hepls render specific routes
+      <Route exact path='/' component={HomePage}/>
+    </Switch>
   </div>
 }
 
