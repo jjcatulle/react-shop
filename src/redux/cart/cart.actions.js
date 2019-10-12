c36bdc4c5728c20ced2792b3283ec6eebd9780bf
Jean Catulle <jjcatulle389@gmail.com>
2019-10-12 16:00:30 -0400

    added addind and removing function in checkout page

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,7 +3,18 @@ import CartActions from "./cart.types";
 export const toggleCartHidden=()=>({
     type:CartActions.TOGGLE_CART_HIDDEN
 });
+
 export const addItemToCart=item=>({
     type:CartActions.ADD_ITEM_TO_CART,
+    payload:item
+});
+
+export const removeItem =item=>({
+    type:CartActions.REMOVE_ITEM,
+    payload:item
+});
+
+export const clearItemFromCart=item=>({
+    type:CartActions.CLEAR_ITEM_FROM_CART,
     payload:item
 });
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,25 +1,40 @@
 import CartActions from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import {
+    addItemToCart,
+    removeItemFromCart
+} from "./cart.utils";
 
-const INITIAL_STATE={
-    hidden:true,
-    cartItems:[]
+const INITIAL_STATE = {
+    hidden: true,
+    cartItems: []
 };
 
-const cartReducer=(state=INITIAL_STATE, action)=>{
+const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case CartActions.TOGGLE_CART_HIDDEN:
-          return  {
+            return {
                 ...state,
-                hidden:! state.hidden,
+                hidden: !state.hidden,
             };
         case CartActions.ADD_ITEM_TO_CART:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActions.REMOVE_ITEM:
             return{
                 ...state,
-                cartItems:addItemToCart(state.cartItems, action.payload)
+                cartItems:removeItemFromCart(state.cartItems,action.payload)
             };
-        default:
-            return state;
+        case CartActions.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems:state.cartItems.filter(
+                    cartItem=>cartItem.id!==action.payload.id
+                )
+            };
+            default:
+                return state;
     }
 }
 
